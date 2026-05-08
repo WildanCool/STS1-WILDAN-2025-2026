4fd81ae8456593f9d84656e2f2335f1674007333
Perdana Muhammad Wildanumukhaladun <160695487+WildanCool@users.noreply.github.com>
2026-05-08 11:20:20 +0700

    updt

--- project_sts_wildan/lib/main.dart
+++ project_sts_wildan/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:project_sts_wildan/STS1_WILDAN/login.dart';
 
 void main() {
@@ -11,12 +13,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'STS1-WILDAN',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      home: AnimatedSplashScreen(
+        splash: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Image.asset('images/splashLogo.png', height: 250)],
+        ),
+        nextScreen: const LoginSTS(),
+        splashIconSize: double.infinity,
+        backgroundColor: const Color.fromARGB(255, 3, 79, 141),
+        duration: 2000,
+        splashTransition: SplashTransition.fadeTransition, 
+        pageTransitionType: PageTransitionType
+            .rightToLeftWithFade, 
+        animationDuration: const Duration(seconds: 1),
       ),
-      home: LoginSTS(),
     );
   }
 }
